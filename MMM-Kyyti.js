8942848f73360f00edac8faa4be10102925ee64b
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-11-21 21:51:39 +0200

    Get route

--- MMM-Kyyti.js
+++ MMM-Kyyti.js
@@ -1,8 +1,16 @@
 /* eslint-disable */
+const tryParseJSON = data => {
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    return data;
+  }
+}
+
 const httpRequest = ({method = 'GET', url, data}) => {
   return new Promise((resolve, reject) => {
     const httpRequest = new XMLHttpRequest();
-    httpRequest.onload = () => resolve(JSON.parse(httpRequest.responseText));
+    httpRequest.onload = () => resolve(tryParseJSON(httpRequest.responseText));
     httpRequest.onerror = () => reject(httpRequest.statusText);
     httpRequest.open(method, url, true);
     httpRequest.setRequestHeader("Content-Type", "application/json");
@@ -17,7 +25,10 @@ Module.register('MMM-Kyyti', {
   start: () => {
     httpRequest({url: env.loginURL, data: credentials, method: 'POST'}).then(res => {
       httpRequest({url: env.myOrdersURL}).then(mitas => {
-        console.log('orderdiiiiit', mitas)
+        console.log('orderdiiiiit', mitas.orders[0])
+        httpRequest({url: `${env.activeRouteURL}/${mitas.orders[0].routeId}`}).then(route => {
+          console.log('route tuli', route.departureTime.time)
+        })
       })
     });
   },
