38de37a45be4cb23400346db21d9faefe45f2dd3
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-11-21 21:35:48 +0200

    Get orders after login

--- MMM-Kyyti.js
+++ MMM-Kyyti.js
@@ -1,18 +1,13 @@
 /* eslint-disable */
-const httpRequest = (url, data) => {
-  const httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = () => {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        console.log(httpRequest.responseText);
-      } else {
-        console.log('There was a problem with the request.', httpRequest.responseText);
-      }
-    }
-  };
-  httpRequest.open('POST', url, true);
-  httpRequest.setRequestHeader("Content-Type", "application/json");
-  httpRequest.send(JSON.stringify(data));
+const httpRequest = ({method = 'GET', url, data, callback}) => {
+  return new Promise((resolve, reject) => {
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.onload = () => resolve(JSON.parse(httpRequest.responseText));
+    httpRequest.onerror = () => reject(httpRequest.statusText);
+    httpRequest.open(method, url, true);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.send(JSON.stringify(data));
+  }); 
 }
 
 Module.register('MMM-Kyyti', {
@@ -20,7 +15,11 @@ Module.register('MMM-Kyyti', {
     return [this.file('./credentials.js'), this.file('./env.js')];
   },
   start: () => {
-    httpRequest(env.loginURL, credentials);
+    httpRequest({url: env.loginURL, data: credentials, method: 'POST'}).then(res => {
+      httpRequest({url: env.myOrdersURL}).then(mitas => {
+        console.log('orderdiiiiit', mitas)
+      })
+    });
   },
   getDom: () => {
     return document.createTextNode("Hello World");
