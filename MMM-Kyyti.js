f6a59b715519b62027102763ad19fccb5f080bc0
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-11-21 21:06:35 +0200

    Send credentials correctly

--- MMM-Kyyti.js
+++ MMM-Kyyti.js
@@ -3,7 +3,6 @@ const httpRequest = (url, data) => {
   const httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = () => {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      console.log('tuli done request')
       if (httpRequest.status === 200) {
         console.log(httpRequest.responseText);
       } else {
@@ -13,8 +12,7 @@ const httpRequest = (url, data) => {
   };
   httpRequest.open('POST', url, true);
   httpRequest.setRequestHeader("Content-Type", "application/json");
-  console.log('responset tulille', data)
-  httpRequest.send(data);
+  httpRequest.send(JSON.stringify(data));
 }
 
 Module.register('MMM-Kyyti', {
@@ -22,8 +20,7 @@ Module.register('MMM-Kyyti', {
     return [this.file('./credentials.js'), this.file('./env.js')];
   },
   start: () => {
-    console.log('STARTING', credentials)
-    httpRequest(env.loginURL, {credentials});
+    httpRequest(env.loginURL, credentials);
   },
   getDom: () => {
     return document.createTextNode("Hello World");
